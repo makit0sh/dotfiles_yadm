d37db90ad12554ca07daa64fe912a300eb8b89a2
makit0sh <t.maki.0402@gmail.com>
2021-04-18 19:08:24 +0900

    added instruction for new install method

--- .config/yadm/github_yadm_bootstrap.sh
+++ .config/yadm/github_yadm_bootstrap.sh
@@ -5,5 +5,4 @@ git clone https://github.com/TheLocehiliosan/yadm.git ~/.yadm-project
 ln -s ~/.yadm-project/yadm ~/bin/yadm
 
 yadm clone https://github.com/makit0sh/dotfiles_yadm
-yadm bootstrap
 
--- .github/README.md
+++ .github/README.md
@@ -1,2 +1,9 @@
 # dotfiles_yadm
 personal dotfiles managed with yadm
+
+## Install
+
+```bash
+bash -c "$(wget -qO- https://git.io/JO0b6)"
+```
+
