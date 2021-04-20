4529297bfd4d826b4d9841cb2643a9b082b5c3e4
makit0sh <t.maki.0402@gmail.com>
2021-04-20 13:14:15 +0900

    fixed github install

--- .config/yadm/github_yadm_bootstrap.sh
+++ .config/yadm/github_yadm_bootstrap.sh
@@ -3,6 +3,7 @@
 mkdir -p ~/bin
 git clone https://github.com/TheLocehiliosan/yadm.git ~/.yadm-project
 ln -s ~/.yadm-project/yadm ~/bin/yadm
+export PATH=~/bin:$PATH
 
 yadm clone https://github.com/makit0sh/dotfiles_yadm
 
