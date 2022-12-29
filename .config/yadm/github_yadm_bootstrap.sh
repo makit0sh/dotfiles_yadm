2ca1010d74e86783b8dda9158c10731566858f1e
makit0sh <t.maki.0402@gmail.com>
2022-12-29 12:41:20 +0900

    Update github_yadm_bootstrap.sh

--- .config/yadm/github_yadm_bootstrap.sh
+++ .config/yadm/github_yadm_bootstrap.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-if ! command -v COMMAND &> /dev/null
-  mkdir -p ~/bin
-  git clone https://github.com/TheLocehiliosan/yadm.git ~/.yadm-project
-  ln -s ~/.yadm-project/yadm ~/bin/yadm
-  export PATH=~/bin:$PATH
+if ! command -v yadm &> /dev/null
+then
+  # debian base system
+  if type apt > /dev/null 2>&1; then
+    sudo apt update
+    sudo apt install -y yadm
+  else
+    mkdir -p ~/bin
+    git clone https://github.com/TheLocehiliosan/yadm.git ~/.yadm-project
+    ln -s ~/.yadm-project/yadm ~/bin/yadm
+    export PATH=~/bin:$PATH
+  fi
 fi
 
 yadm clone https://github.com/makit0sh/dotfiles_yadm
-
